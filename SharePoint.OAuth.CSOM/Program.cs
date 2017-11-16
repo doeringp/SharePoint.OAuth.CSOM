0d00a39ac27bff699113ebd57b309bc98b1a118c
Peter Doering <mail@peter-doering.com>
2017-11-16 22:43:48 +0100

    Add project files.

---
+++ SharePoint.OAuth.CSOM/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace SharePoint.OAuth.CSOM
+{
+    using Microsoft.SharePoint.Client;
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri targetApplicationUri = new Uri("https://mytenantid.sharepoint.com/sites/mywebsitename");
+            
+            string targetRealm = TokenHelper.GetRealmFromTargetUrl(targetApplicationUri);
+
+            var accessToken = TokenHelper.GetAppOnlyAccessToken
+                (TokenHelper.SharePointPrincipal, targetApplicationUri.Authority, targetRealm).AccessToken;
+                
+            //we use the app-only access token to authenticate without the interaction of the user
+            using (ClientContext context = TokenHelper.GetClientContextWithAccessToken(targetApplicationUri.ToString(), accessToken))
+            {
+                Web web = context.Web;
+
+                context.Load(web);
+                context.ExecuteQuery();
+                Console.WriteLine(web.Title);
+
+                List list = context.Web.Lists.GetByTitle("MyListName");
+                CamlQuery query = CamlQuery.CreateAllItemsQuery(100);
+                ListItemCollection items = list.GetItems(query);
+                context.Load(items);
+                context.ExecuteQuery();
+                foreach (ListItem item in items)
+                {
+                    Console.WriteLine(item["Title"]);
+                }
+            }
+        }
+    }
+}
